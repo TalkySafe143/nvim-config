0d42fa92ef9e7e872f00b48a503848bf335375c5
Aulas ingenieria <aulasingenieria@12C302E06190847.ujaverianas.loc>
2025-02-17 17:02:19 -0500

    Fix ensure installed

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,3 +1,19 @@
+local servers = {
+    "lua_ls",
+    "clangd",
+    "ts_ls",
+    "astro",
+    "csharp_ls",
+    "cssls",
+    "dockerls",
+    "html",
+    "jdtls",
+    "pyright",
+    "texlab",
+    "buf_ls",
+    "cmake",
+    "tailwindcss",
+}
 return {
 	{
 		"williamboman/mason.nvim",
@@ -10,11 +26,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"clangd",
-					"lua_ls",
-					"ts_ls",
-				},
+				ensure_installed = servers;
 			})
 		end,
 	},
@@ -24,22 +36,7 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 
-            local servers = {
-                "lua_ls",
-                "clangd",
-                "ts_ls",
-                "astro",
-                "csharp_ls",
-                "cssls",
-                "dockerls",
-                "html",
-                "jdtls",
-                "pyright",
-                "texlab",
-                "buf_ls",
-                "cmake",
-                "tailwindcss",
-            }
+            
 
             for _, server in ipairs(servers) do
                 lspconfig[server].setup({
@@ -47,15 +44,6 @@ return {
                 })
             end
 
-			--lspconfig.lua_ls.setup({
-				--capabilities = capabilities,
-			-- })
-			--lspconfig.clangd.setup({
-				--capabilities = capabilities,
-			-- })
-			--lspconfig.ts_ls.setup({
-				--capabilities = capabilities,
-			-- })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
