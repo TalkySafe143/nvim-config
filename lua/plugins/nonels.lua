2d2bf142eb168b4f280a2d1e4be23eb4efb756c2
Sebastian Galindo <sebastian.galindo143@gmail.com>
2026-02-23 10:38:49 -0500

    Adding CppLint

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -1,17 +1,18 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.stylua,
-			},
-		})
-		vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
-	end,
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.clang_format,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.diagnostics.cpplint,
+            },
+        })
+        vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
+    end,
 }
