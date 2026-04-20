6d6fc81ac3ac03ce90f9d7db4e7958683fe8e47c
Sebastian Galindo <sebastian.galindo143@gmail.com>
2026-04-19 23:32:35 -0400

    Fix: nvim-treesitter mantainer just gave up :(

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,6 @@
 return {
-    "nvim-treesitter/nvim-treesitter", 
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    config = function()
-         local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          auto_install = true,
-          sync_install = false,
-          highlight = { 
-              enable = true,
-              additional_vim_regex_highlighting = false 
-          },
-        })
-    end
 }
