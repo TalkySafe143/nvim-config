fdcee468c8510bb8aa810321292590268a88b870
Sebastian Galindo <sebastian.galindo143@gmail.com>
2026-04-20 19:39:14 -0400

    Fix: Telescope version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,13 @@
 return {
     {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        "nvim-telescope/telescope.nvim",
+        version = "*",
+        dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local builtin = require("telescope.builtin")
-            vim.keymap.set('n', '<leader>fi', builtin.find_files, { desc = 'Telescope find files' })
-            vim.keymap.set('n', '<leader>fs', builtin.lsp_dynamic_workspace_symbols, {})
+            vim.keymap.set("n", "<leader>fi", builtin.find_files, { desc = "Telescope find files" })
+            vim.keymap.set("n", "<leader>fi", builtin.find_files, { desc = "Telescope find files" })
+            vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
             require("telescope").setup({
                 defaults = {
                     layout_config = {
@@ -16,22 +17,19 @@ return {
                     },
                 },
             })
-        end
+        end,
     },
     {
-        'nvim-telescope/telescope-ui-select.nvim',
+        "nvim-telescope/telescope-ui-select.nvim",
         config = function()
-            require("telescope").setup {
-              extensions = {
-                ["ui-select"] = {
-                  require("telescope.themes").get_dropdown {
-                  }
-                }
-              }
-            }
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
             require("telescope").load_extension("ui-select")
-        end
-    }
-
+        end,
+    },
 }
-
