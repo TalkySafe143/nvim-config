4592f11cd1154861e749356321c51ed45228cdc6
Sebastian Galindo <sebastian.galindo143@gmail.com>
2026-07-22 17:39:45 -0400

    add: conform as formatter

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -20,15 +20,14 @@ return {
 		})
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.formatting.clang_format,
+				-- null_ls.builtins.formatting.prettier,
+				-- null_ls.builtins.formatting.black,
+				-- null_ls.builtins.formatting.isort,
+				-- null_ls.builtins.formatting.stylua,
 				mail_formatter,
 			},
 		})
-		vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
 	end,
 }
